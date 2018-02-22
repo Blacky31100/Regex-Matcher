c0490a6f3fd4f262f24568018a06557f94b404d2
q.faure <qfaure@outlook.com>
2018-02-22 15:05:21 +0100

    Init project

---
+++ QF.Regex.Matcher.Core/QF.Regex.Matcher.Core/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QF.Regex.Matcher.Core
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
